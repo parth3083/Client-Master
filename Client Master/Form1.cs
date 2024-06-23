22d4f35575e53de440749664cbaa6420b6a1a455
parth3083 <parthbrajput30@gmail.com>
2024-06-23 21:26:32 +0530

    Add project files.

---
+++ Client Master/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_Master
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numberOfEntries;
+            if (int.TryParse(textBox1.Text, out numberOfEntries) && numberOfEntries > 0)
+            {
+                for (int i = 0; i < numberOfEntries; i++)
+                {
+                    var entryForm = new EntryForm();
+                    entryForm.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number of entries.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var updateForm = new UpdateForm();
+            updateForm.ShowDialog();
+        }
+    }
+}
